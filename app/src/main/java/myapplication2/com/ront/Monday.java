72654140444bc91f6850fe3f1e808438d4c67004
pratikwagh <pratik.s.wagh95@gmail.com>
2018-03-21 15:48:47 +0530

    sixth commit

--- app/src/main/java/myapplication2/com/ront/Monday.java
+++ app/src/main/java/myapplication2/com/ront/Monday.java
@@ -1,6 +1,8 @@
 package myapplication2.com.ront;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +24,17 @@ public class Monday extends Fragment
         // keep the fragment and all its data across screen rotation
         setRetainInstance(true);
         View rootView = inflater.inflate(R.layout.fragment_monday, container, false);
+
+        FloatingActionButton fab = rootView.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent a = new Intent(getActivity(), RoutineCreate.class);
+                startActivity(a);
+
+            }
+        });
 
         return rootView;
     }
---
+++ app/src/main/java/myapplication2/com/ront/RoutineCreate.java
@@ -0,0 +1,18 @@
+package myapplication2.com.ront;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 21/3/18.
+ */
+
+public class RoutineCreate extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routine_create);
+
+    }
+}
